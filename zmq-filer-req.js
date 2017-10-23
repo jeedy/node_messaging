d676c41f5a469d7cb071eb8e3805cc60eaa82ecf
jee yong kim <jeeyongkim@jeeui-MacBook-Pro.local>
2017-10-23 11:11:55 +0900

    first commit

---
+++ zmq-filer-req.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const
+    zmq = require('zmq'),
+    filename = process.argv[2],
+
+    requester = zmq.socket('req');
+
+requester.on("meessage", function(data){
+    let response = JSON.parse(data);
+    console.log("Received response: ", response);
+
+});
+
+requester.connect("tcp://localhost:5433");
+
+console.log('sending request for ', filename);
+
+requester.send(JSON.stringify({
+    path: filename
+}));
+
